4b9bc510c05d734f0a5d398bf4cb02ffc7fb3771
Artur Drummond <arturbdr@gmail.com>
2025-08-08 17:38:02 +0200

    Refactor Jenkinsfile to support environment-specific template registration and add pipeline configuration

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,24 +1,24 @@
 libraries {
-  rest
+    sdp
 }
 
 application_environments {
-  dev {
-    short_name = "dev"
-    document_service_url = "https://f0ed9cc2a917.ngrok-free.app"
-  }
-  staging {
-    short_name = "staging"
-    document_service_url = "https://f0ed9cc2a917.ngrok-free.app"
-  }
-  prod {
-    short_name = "prod"
-    document_service_url = "https://f0ed9cc2a917.ngrok-free.app"
-  }
+    dev {
+        short_name = "dev"
+        document_service_url = "https://f0ed9cc2a917.ngrok-free.app"
+    }
+    staging {
+        short_name = "staging"
+        document_service_url = "https://f0ed9cc2a917.ngrok-free.app"
+    }
+    prod {
+        short_name = "prod"
+        document_service_url = "https://f0ed9cc2a917.ngrok-free.app"
+    }
 }
 
 keywords {
-  develop = /^develop$/
-  main = /^main$/
-  production = /^v\d+\.\d+\.\d+/
+    develop = /^develop$/
+    main = /^main$/
+    production = /^v\d+\.\d+\.\d+/
 }
